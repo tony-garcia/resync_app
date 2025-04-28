aadc051d3e51a57547d59b4f8302b3e32ecc984c
Tony Garcia <tony@garcia.me>
2025-04-27 22:00:56 -0400

    Validate that a valid SMILES string was entered

--- api/compound_manager/serializers.py
+++ api/compound_manager/serializers.py
@@ -2,7 +2,9 @@
 """
 Django REST framework serializers
 """
+from rdkit import Chem
 from rest_framework import serializers
+
 from .models import Compound
 
 
@@ -12,7 +14,12 @@ class CompoundSerializer(serializers.ModelSerializer):
         fields = ["compound_id", "smiles", "data"]
 
     def validate(self, attrs):
-        """Validates that any compound data is either a string, int, or float"""
+        """Validates syntactically valid SMILES and that any compound data is either a string, int, or float"""
+        errors = {}
+        if attrs.get("smiles"):
+            mol = Chem.MolFromSmiles(attrs.get("smiles"), sanitize=False)
+            if mol is None:
+                errors["smiles"] = "Invalid SMILES string entered"
         if attrs.get("data"):
             compound_data = attrs.get("data")
             allowed_types = (str, int, float)
@@ -21,9 +28,9 @@ class CompoundSerializer(serializers.ModelSerializer):
                 if not isinstance(value, allowed_types):
                     invalid_keys.append(key)
             if invalid_keys:
-                raise serializers.ValidationError(
-                    {
-                        "data": f"Compound data must be strings, integers, or floats. The following were not: {', '.join(invalid_keys)}"
-                    }
+                errors["data"] = (
+                    f"Compound data must be strings, integers, or floats. The following were not: {', '.join(invalid_keys)}"
                 )
+        if errors:
+            raise serializers.ValidationError(errors)
         return super().validate(attrs)
