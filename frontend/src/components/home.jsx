585c9bd3d4638bfaa4b3182a38521d1f1cda86a6
Tony Garcia <tony@garcia.me>
2025-05-01 20:26:34 -0400

    Add Material UI and dependencies

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -1,10 +1,12 @@
 import { useAPI } from "../../hooks";
 
+import Typography from '@mui/material/Typography';
+
 function Home() {
 
   const { data, isLoading, error } = useAPI({ url:'/api/compounds/' }, {});
 
-  return <p>{ isLoading ? 'Hello' : error ? 'An error has occurred' : 'Data has loaded!' }</p>;
+  return <Typography>{ isLoading ? 'Hello' : error ? 'An error has occurred' : 'Data has loaded!' }</Typography>;
 }
 
 export default Home;
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,5 +1,11 @@
 import { createRoot } from 'react-dom/client';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 
